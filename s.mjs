db78126b15e965c7cb5eb246d24dc4a2680f2016
LitvinovKV <SquertelSquertel@mail.ru>
2017-10-19 21:31:31 +0300

    third

--- s.mjs
+++ s.mjs
@@ -1,19 +1,15 @@
 import express from 'express';
-const PORT=4321;
-express()
-  .use(express.static('public'))
-  .get('/', r=>r.res.sendFile('public/index.html'))
-  .use(r=>r.res.status(404).end('Still not here, sorry!'))
-  .use((e,r,res,n)=>res.status(500).end(`Error: ${e}`))
-  .listen(process.env.PORT || PORT, ()=>console.log(process.pid))
-;
-
 const PORT=4321,
       CORS = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET,POST,DELETE',
         'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
-      }
+      };
+express()
+  .use(express.static('public'))
+  .get('/author', r=>r.res.set(CORS).send('Кирилл Литвинов') )
+  .get('/', r=>r.res.sendFile('public/index.html'))
+  .use(r=>r.res.status(404).end('Still not here, sorry!'))
+  .use((e,r,res,n)=>res.status(500).end(`Error: ${e}`))
+  .listen(process.env.PORT || PORT, ()=>console.log(process.pid))
 ;
-
-.get('/author', r=>r.res.set(CORS).send('Кирилл Литвинов'))
