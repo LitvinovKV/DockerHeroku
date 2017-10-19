6ee3a8d91d9704c17a5e5734ebd1ce2820737297
LitvinovKV <SquertelSquertel@mail.ru>
2017-10-19 21:13:18 +0300

    second

--- s.mjs
+++ s.mjs
@@ -1,18 +1,19 @@
 import express from 'express';
-const PORT=4321,
- CORS = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,POST,DELETE',
-        'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
-      }
-;
-
+const PORT=4321;
 express()
   .use(express.static('public'))
   .get('/', r=>r.res.sendFile('public/index.html'))
   .use(r=>r.res.status(404).end('Still not here, sorry!'))
   .use((e,r,res,n)=>res.status(500).end(`Error: ${e}`))
   .listen(process.env.PORT || PORT, ()=>console.log(process.pid))
-
-	.get('/author', r=>r.res.set(CORS).send('Алан Тьюринг'))
 ;
+
+const PORT=4321,
+      CORS = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+        'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+      }
+;
+
+.get('/author', r=>r.res.set(CORS).send('Кирилл Литвинов'))
